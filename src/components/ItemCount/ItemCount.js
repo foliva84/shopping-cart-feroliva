5a50eb0b22a9c841429026c84767d416f7b39c6b
Fer Oliva <fernandogermanoliva@gmail.com>
2022-10-15 17:54:34 -0300

    Se agrega componente ItemCount

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,37 @@
+import React, { useState }from 'react';
+
+const ItemCount = ({ stock, initial, onAdd }) => {
+    const [count, setCount] = useState(initial);
+
+    const substract = () => {
+        if (count > 1) {
+            setCount(count - 1);
+        }
+    }
+
+    const add = () => {
+        if (count < stock) {
+            setCount(count + 1);
+        }
+    }
+
+    // const addToCart = () => {
+    //     onAdd(count);
+    // };
+
+    return (
+        <>
+            <button onClick={substract}>-</button>
+            <h2>{count}</h2>
+            <button onClick={add}>+</button>
+            <button disabled={stock===0} onClick={ () => onAdd(count) }>
+                <span>{ stock === 0 ? "No hay stock" : "Agregar al carrito" }</span>
+            </button>
+            {/* <button disabled={stock===0} onClick={addToCart}>
+                <span>{ stock === 0 ? "No hay stock" : "Agregar al carrito" }</span>
+            </button> */}
+        </>
+    )
+}
+
+export default ItemCount
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import ItemCount from '../ItemCount/ItemCount'
+
 
 const ItemListContainer = ({ greeting }) => {
+    const onAdd = (count) => {
+        console.log(`El usuario quiere agregar ${count} productos`);
+    };
+
     return (
-        <h4>{greeting}</h4>
+        <>
+            <h4>{greeting}</h4>
+            <ItemCount 
+                onAdd={onAdd}
+                initial={1}
+                stock={0}
+                />
+        </>
     )
 }
 
