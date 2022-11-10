621a9643aada3039a9296507c75ffda7405fce6a
Fer Oliva <fernandogermanoliva@gmail.com>
2022-11-10 20:31:59 -0300

    Agregando lógica del desafío

--- src/Containers/ItemDetail/ItemDetail.js
+++ src/Containers/ItemDetail/ItemDetail.js
@@ -1,15 +1,23 @@
-import React from 'react'
+ import React from "react";
+import ItemCount from "../../Components/ItemCount/ItemCount";
 
 const ItemDetail = ({ item }) => {
     const {title, image, description, price} = item;
 
+    const onAdd = (count) => {
+      console.log(`El usuario quiere agregar ${count} productos`);
+    };
+
     return (
+       <>
         <div>
           <img alt={title} src={image} />
           <h1>{title}</h1>
           <span>{description}</span>
-          <h2>{price}</h2>
+          <h2>${price}</h2>
         </div>
+        <ItemCount initial={1} stock={3} onAdd={ onAdd } />
+        </>
       );
 }
 
--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,7 +1,9 @@
 import React, { useState }from 'react';
+import { Link as LinkRRD } from "react-router-dom";
 
 const ItemCount = ({ stock, initial, onAdd }) => {
     const [count, setCount] = useState(initial);
+    const [finalizarCompra, setFinalizarCompra] = useState(false);
 
     const substract = () => {
         if (count > 1) {
@@ -15,21 +17,33 @@ const ItemCount = ({ stock, initial, onAdd }) => {
         }
     }
 
-    // const addToCart = () => {
-    //     onAdd(count);
-    // };
+    const addToCart = () => {
+        setFinalizarCompra(true);
+        onAdd(count);
+    };
 
     return (
         <>
-            <button onClick={substract}>-</button>
-            <h2>{count}</h2>
-            <button onClick={add}>+</button>
-            <button disabled={stock===0} onClick={ () => onAdd(count) }>
-                <span>{ stock === 0 ? "No hay stock" : "Agregar al carrito" }</span>
-            </button>
-            {/* <button disabled={stock===0} onClick={addToCart}>
-                <span>{ stock === 0 ? "No hay stock" : "Agregar al carrito" }</span>
-            </button> */}
+            {
+                (!finalizarCompra ? 
+                    (
+                        <>
+                        <button onClick={substract}>-</button>
+                        <h2>{count}</h2>
+                        <button onClick={add}>+</button>                        
+                        <button disabled={ stock === 0 } onClick={ () => addToCart() }>
+                            <span>{ stock === 0 ? "No hay stock" : "Agregar al carrito" }</span>
+                        </button>
+                        </>
+                    ): 
+                        <LinkRRD to={"/cart/"}>
+                            <button className="btn btn-secondary botonesProductos">Finalizar Compra</button>
+                        </LinkRRD>        
+                    )
+            }
+            {/* // <button disabled={ stock === 0 } onClick={ () => addToCart() }>
+            //     <span>{ stock === 0 ? "No hay stock" : "Agregar al carrito" }</span>
+            // </button> */}
         </>
     )
 }
